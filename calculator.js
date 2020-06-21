a38702211e18913ccb103fd5113ece25850fbf3b
Geoff Chambon <chambg02@gmail.com>
2020-06-21 21:31:06 +0800

    A start

---
+++ calculator.js
@@ -0,0 +1,23 @@
+
+
+function add(a,b) {
+	var ans = a + b;
+	return ans;
+}
+
+function substract(a,b) {
+	var ans = a - b;
+	return ans;
+}
+
+function multiply(a,b) {
+	var ans = a * b;
+	return ans;
+}
+
+function divide(a,b) {
+	var ans = a / b;
+	return ans;
+}
+
+
