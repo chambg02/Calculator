a493785f3f33e171ed6d56f6b6f86c0dd6570315
Geoff Chambon <chambg02@gmail.com>
2020-06-25 22:34:36 +0800

    Added functionality to display in console value of button pressed.

--- calculator.js
+++ calculator.js
@@ -1,5 +1,15 @@
 
 
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+	button.addEventListener('click', (e) => {
+		const buttonSelect = button.value;
+		console.log(buttonSelect);
+		OnClick="document.getElementById('diplayResult').value += buttonSelect";
+});
+});
+
+
 function add(a,b) {
 	var ans = a + b;
 	return ans;
