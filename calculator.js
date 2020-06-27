6245a20066a6a29e8c157b52b6ae8ad191e72af5
Geoff Chambon <chambg02@gmail.com>
2020-06-27 21:34:15 +0800

    works with eval(), now to replace eval with operate function.

--- calculator.js
+++ calculator.js
@@ -1,13 +1,21 @@
 
-
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
 	button.addEventListener('click', (e) => {
 		const buttonSelect = button.value;
 		console.log(buttonSelect);
-		OnClick="document.getElementById('diplayResult').value += buttonSelect";
+		if (button.value == "=") {
+			let evaluate = eval(displayResult.value);
+			console.log(evaluate);
+			displayResult.value = evaluate;
+		} else if (button.value == "C") {
+			window.location.reload();
+		}
+
 });
 });
+
+
 
 
 function add(a,b) {
