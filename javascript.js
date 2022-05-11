e9bd7953bf0226b4af95e3acaff4336fd16398aa
chambg02 <chambg02@gmail.com>
2022-05-11 19:50:51 +0800

    Update to react to work correctly with 2 numbers and 1 operator only.

--- javascript.js
+++ javascript.js
@@ -1,39 +1,50 @@
 
 // DOM manipulation to add numbers in display
 
-const select = document.querySelector("#display");
 
+// this currently does nothing
 let buttonClickNumber = document.getElementsByClassName("numBtn");
 let buttonClickOperator = document.getElementsByClassName("opsBtn");
-/*
-buttonClick.forEach(button => {
-    button.addEventListener('click', event => {
-        display(button.data-id);
-    })
-})
 
-function display(input) {
-    select.append(input);
+//set global
+const select = document.querySelector("#display");
+let storeDisplayValue = "";
+let numOne = 0;
+let numTwo = 0;
+let numbers = 0;
+let ope = "";
+let total = 0;
+
+//record button presses and displays/stores
+function displayValue(x) {
+    select.append(x);
+    storeDisplayValue = storeDisplayValue + x;
+    console.log(storeDisplayValue);
+    return storeDisplayValue;
 }
 
-*/
-
-
-
+//analyses/splits stored data and runs through operate function
+function equalPress(storeDisplayValue) {
+    numbers = storeDisplayValue.split(/[*]|[+]|[-]|[\/]/);
+    operators = storeDisplayValue.split(/[^-|+|\/|\*]/).filter(e => e);
+    numOne = parseInt(numbers[0]);
+    numTwo = parseInt(numbers[1]);
+    ope = operators[0];
+    total = operate(parseInt(numOne), parseInt(numTwo), ope);
+    let newRow = document.createElement("div");
+    select.appendChild(newRow);
+    newRow.textContent = '= ' + `${total}`;
+}
 
 // carry out operations
 function operate(numOne, numTwo, ope) {
-    if (ope === "add") {
-        console.log("add");
+    if (ope === '+') {
         return add(numOne,numTwo);
-    }  else if (ope === "min") {
-        console.log("min");
+    }  else if (ope === "-") {
         return subtract(numOne, numTwo);
-    }  else if (ope === "mult") {
-        console.log("mult");
+    }  else if (ope === '*') {
         return multiply(numOne, numTwo);
-    }  else if (ope === "div") {
-        console.log("div");
+    }  else if (ope === "/") {
         return divide(numOne, numTwo)
     }
 }
